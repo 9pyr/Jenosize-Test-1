5665d184d5e03f989b1173cc256a641712e4d9f1
9Pyr <9pyra.s@gmail.com>
2022-01-17 15:06:17 +0700

    update find count_points

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import dataMock, { findCommentByPostId, findLikesByPostId, findUserById } from '__mock/actions'
+import dataMock, { findCommentByPostId, findLikesByPostId, findPointByPostId, findUserById } from '__mock/actions'
 
 const { posts: initialPosts } = dataMock
 export default function handler(req, res) {
@@ -7,6 +7,7 @@ export default function handler(req, res) {
     const user = findUserById(post.user_id)
     const likes = findLikesByPostId(post.id)
     const comments = findCommentByPostId(post.id)
+    const points = findPointByPostId(post.id)
 
     return {
       id: post.id,
@@ -17,6 +18,7 @@ export default function handler(req, res) {
       company: user.company,
       count_likes: likes.length,
       count_comments: comments.length,
+      count_points: points.length,
     }
   })
 
